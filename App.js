143ce1f3d01154c282c587da13b42a38841fd29d
snvn1511 <spxedu@gmail.com>
2023-01-06 18:16:34 +0700

    b2

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import DemoComp from './components/DemoComp';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello</Text>
       <StatusBar style="auto" />
+
+      <DemoComp />
+      <DemoComp />
+      <DemoComp />
+      <DemoComp />
+      <DemoComp />
+      <DemoComp />
+      
     </View>
   );
 }
@@ -13,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/DemoComp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const DemoComp = ()=>{
+    return (
+        <View>
+            <Text>
+                Nội dung DemoComp 
+            </Text>
+        </View>
+
+    );
+}
+
+export default DemoComp;
